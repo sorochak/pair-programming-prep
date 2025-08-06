16979053ba121a037488e5da572aa08b52e9a9a4
Austen Sorochak <austensorochak@gmail.com>
2025-08-05 19:20:52 -0700

    update imports and add cors

--- express-app/server.js
+++ express-app/server.js
@@ -1,7 +1,14 @@
 // express-app/index.js
 const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { fetchAndMergeData } = require("./services/fetchChargers");
+
 const app = express();
 const PORT = 3000;
+
+// Enable CORS for all origins
+app.use(cors());
 
 app.get("/chargers", (req, res) => {
   res.json([{ id: "A1", status: "online", lastSeen: Date.now() }]);
